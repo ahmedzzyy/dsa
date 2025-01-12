d805a87c89ddfd16f9762b07cb688fcfd244c411
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-01-12 22:37:36 +0530

    Hash table using Chaining (D.S.) implemented with Kt

---
+++ src/main/kotlin/datastructures/hashtable/ChainingHashTable.kt
@@ -0,0 +1,95 @@
+package edu.practice.datastructures.hashtable
+
+import kotlin.math.abs
+
+class ChainingHashTable<K, V> {
+
+    companion object {
+        private data class Entry<K, V>(val key: K, var value: V, var next: Entry<K, V>? = null)
+
+        private const val INITIAL_CAPACITY = 16
+        private const val LOAD_FACTOR_THRESHOLD = 0.7
+    }
+
+    private var table: Array<Entry<K, V>?> = arrayOfNulls(INITIAL_CAPACITY)
+    private var size = 0
+
+    private fun hash(key: K): Int {
+        return abs(key.hashCode() % table.size)
+    }
+
+    fun put(key: K, value: V) {
+        if (size.toDouble() / table.size >= LOAD_FACTOR_THRESHOLD) {
+            resize()
+        }
+
+        val index = hash(key)
+        var entry = table[index]
+
+        while (entry != null) {
+            if (entry.key == key) {
+                entry.value = value // Update existing key's value
+                return
+            }
+
+            entry = entry.next
+        }
+
+        val prevEntry = table[index]
+        table[index] = Entry(key, value, prevEntry)
+        size++
+    }
+
+    fun get(key: K): V? {
+        val index = hash(key)
+        var entry = table[index]
+        while (entry != null) {
+            if (entry.key == key) {
+                return entry.value
+            }
+
+            entry = entry.next
+        }
+
+        return null
+    }
+
+    fun remove(key: K): Boolean {
+        val index = hash(key)
+        var entry = table[index]
+        var prev: Entry<K, V>? = null
+        while (entry != null) {
+            if (entry.key == key) { // Removal
+                if (prev != null) {
+                    prev.next = entry.next
+                } else {
+                    table[index] = entry.next
+                }
+
+                size--
+                return true
+            }
+
+            prev = entry
+            entry = entry.next
+        }
+
+        return false
+    }
+
+    private fun resize() {
+        val newCapacity = table.size * 2
+        val oldTable: Array<Entry<K, V>?> = table
+
+        table = arrayOfNulls(newCapacity)
+        size = 0
+
+        for (entry in oldTable) {
+            entry?.let { put(it.key, it.value) }
+        }
+    }
+
+    fun size(): Int = size
+
+    fun isEmpty(): Boolean = (size == 0)
+}
---
+++ src/test/kotlin/edu/practice/datastructures/hashtable/ChainingHashTableTest.kt
@@ -0,0 +1,87 @@
+package edu.practice.datastructures.hashtable
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class ChainingHashTableTest {
+
+    private lateinit var hashTable: ChainingHashTable<String, Int>
+
+    @BeforeEach
+    fun setup() {
+        hashTable = ChainingHashTable()
+    }
+
+    @Test
+    fun hashTableIsInitializedAsEmpty() {
+        assertThat(hashTable.isEmpty()).isTrue()
+        assertThat(hashTable.size()).isZero()
+    }
+
+
+    // Put tests indirectly tests Get method
+    @Test
+    fun putAnElementInTable() {
+        val key = "key1"
+        val `val` = 50
+        hashTable.put(key, `val`)
+
+        assertThat(hashTable.isEmpty()).isFalse()
+        assertThat(hashTable.size()).isOne()
+        assertThat(hashTable.get(key)).isEqualTo(`val`)
+    }
+
+    @Test
+    fun updateExistingElementInTable() {
+        val key = "key1"
+        val `val` = 50
+        hashTable.put(key, `val`)
+        val newVal = 60
+        hashTable.put(key, newVal)
+
+        assertThat(hashTable.get(key)).isNotEqualTo(`val`)
+        assertThat(hashTable.get(key)).isEqualTo(newVal)
+    }
+
+    @Test
+    fun putMultipleElementsInTable() {
+        val vals = intArrayOf(50, 60, 70)
+        val keys = arrayOf("key1", "key2", "key3")
+        for (i in keys.indices) {
+            hashTable.put(keys[i], vals[i])
+        }
+
+        for (i in keys.indices) {
+            assertThat(hashTable.get(keys[i])).isEqualTo(vals[i])
+        }
+    }
+
+    @Test
+    fun checkIfTableIntactAfterResizing() {
+        val lengthForInsert = 14
+        for (i in 0..<lengthForInsert) {
+            hashTable.put("key$i", i)
+        }
+
+        assertThat(hashTable.size()).isEqualTo(lengthForInsert)
+        assertThat(hashTable.get("key12")).isEqualTo(12)
+    }
+
+    @Test
+    fun getNonExistentElementInTable() {
+        assertThat(hashTable.get("Non Existent Key")).isNull()
+    }
+
+    @Test
+    fun removeAnElementInTable() {
+        val vals = intArrayOf(50, 60, 70)
+        val keys = arrayOf("key1", "key2", "key3")
+        for (i in keys.indices) {
+            hashTable.put(keys[i], vals[i])
+        }
+
+        assertThat(hashTable.remove(keys[1])).isTrue()
+        assertThat(hashTable.get(keys[1])).isNull()
+    }
+}
