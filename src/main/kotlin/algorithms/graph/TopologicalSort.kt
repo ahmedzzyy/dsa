f1aae6b3edcfece264390df78c075c0af3f4b51a
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-01-25 02:54:47 +0530

    Topological Sort graph (Algorithms) implemented in Kt

---
+++ src/main/kotlin/algorithms/graph/TopologicalSort.kt
@@ -0,0 +1,89 @@
+package edu.practice.algorithms.graph
+
+import edu.practice.datastructures.graph.Graph
+
+/**
+ * Performs a topological sort on a directed acyclic graph (DAG).
+ *
+ *  Topological sorting is an ordering of the vertices in a directed graph such that for every directed edge (u, v),
+ *  vertex `u` comes before vertex `v` in the ordering. This function assumes the input graph is directed and acyclic.
+ *
+ * @param E The type of vertices in the graph.
+ * @param graph The directed acyclic graph (DAG) for which the topological sort will be performed
+ * @return A list of vertices in topological order.
+ *
+ * @throws IllegalArgumentException If the input graph contains a cycle or is not directed. Topological sorting is
+ * defined only for directed acyclic graphs.
+ */
+fun <E> topologicalSort(graph: Graph<E>): List<E> {
+    if (!graph.isDirected) throw IllegalArgumentException(
+        "Topological sort is only valid for directed acyclic graphs (DAGs). The input graph is undirected."
+    )
+    if (isCyclicGraph(graph)) throw IllegalArgumentException(
+        "Topological sort is only valid for directed acyclic graphs (DAGs). The input graph contains a cycle."
+    )
+
+    val visitedSet: MutableSet<E> = mutableSetOf()
+    val topSort = ArrayDeque<E>()
+
+    fun dfsVisit(vertex: E) {
+        visitedSet.add(vertex)
+
+        // Graph is Directed, so get children only not all neighbors
+        val neighbors = graph.getChildren(vertex)
+
+        for (neighbor in neighbors) {
+            if (!visitedSet.contains(neighbor)) {
+                visitedSet.add(neighbor)
+                dfsVisit(neighbor)
+            }
+        }
+
+        topSort.addFirst(vertex)
+    }
+
+    for (vertex in graph.getVertices()) {
+        if (!visitedSet.contains(vertex)) {
+            dfsVisit(vertex)
+        }
+    }
+
+    return topSort
+}
+
+private fun <E> isCyclicGraph(graph: Graph<E>): Boolean {
+    val visitedSet: MutableSet<E> = mutableSetOf()
+    val currentPathStack = ArrayDeque<E>()
+
+    fun dfsVisit(vertex: E): Boolean {
+        // If the vertex is already in the recursion stack, a cycle is detected
+        if (currentPathStack.contains(vertex)) return true
+
+        // If the vertex is already visited, no cycle from this vertex
+        if (visitedSet.contains(vertex)) return false
+
+        visitedSet.add(vertex)
+        currentPathStack.addLast(vertex)
+
+        val neighbors = if (graph.isDirected) {
+            graph.getChildren(vertex)
+        } else {
+            graph.getNeighbors(vertex)
+        }
+
+        for (neighbor in neighbors) {
+            if (dfsVisit(neighbor)) return true
+        }
+
+        currentPathStack.removeLast()
+        return false
+    }
+
+    for (vertex in graph.getVertices()) {
+        if (!visitedSet.contains(vertex)) {
+            if (dfsVisit(vertex)) return true
+        }
+    }
+
+    return false
+}
--- src/test/kotlin/edu/practice/algorithms/graph/GraphAlgorithmsTest.kt
+++ src/test/kotlin/edu/practice/algorithms/graph/GraphAlgorithmsTest.kt
@@ -16,6 +16,56 @@ class GraphAlgorithmsTest {
     }
 
     @Test
+    fun topologicalSortGeneratedForGraph() {
+        val names = arrayOf("AAA", "BCD", "EFG", "HIJ", "KLM", "NOP")
+        for (name in names) {
+            nameGraph.addVertex(name)
+        }
+
+        nameGraph.addEdge(names[0], names[1])
+        nameGraph.addEdge(names[2], names[0])
+        nameGraph.addEdge(names[3], names[5])
+        nameGraph.addEdge(names[1], names[3])
+        nameGraph.addEdge(names[2], names[4])
+
+//        starts from 3rd  i.e. "HIJ"
+//        names[3] to (1 to 4),
+//        names[5] to (2 to 3), // 3 -> 5
+//        names[0] to (5 to 8), // standalone 0 from `for`
+//        names[1] to (6 to 7), // 0 -> 1
+//        names[4] to (9 to 10), // standalone 4 from `for` (if 2 occurred earlier, it would be different)
+//        names[2] to (11 to 12) // standalone 2 from `for`
+
+        assertThat(topologicalSort(nameGraph)).containsExactly(
+            "EFG", "KLM", "AAA", "BCD", "HIJ", "NOP"
+            // These are sorted descending by finishing time
+        )
+    }
+
+    @Test
+    fun topologicalSortThrowsErrorForUnDirectedOrCyclicGraph() {
+        val names = arrayOf("AAA", "BCD", "EFG", "HIJ", "KLM", "NOP")
+        for (name in names) {
+            nameGraph.addVertex(name)
+        }
+
+        nameGraph.addEdge(names[1], names[0])
+        nameGraph.addEdge(names[0], names[2])
+        nameGraph.addEdge(names[2], names[1])
+        nameGraph.addEdge(names[0], names[3])
+        nameGraph.addEdge(names[3], names[4])
+
+        assertThatThrownBy { topologicalSort(nameGraph) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("Topological sort is only valid for directed acyclic graphs (DAGs). The input graph contains a cycle.")
+
+        val undirectedGraph = Graph.create<Int>()
+        assertThatThrownBy { topologicalSort(undirectedGraph) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessageContaining("Topological sort is only valid for directed acyclic graphs (DAGs). The input graph is undirected.")
+    }
+
+    @Test
     fun findStronglyConnectedComponentsByKosarajuAlgorithm() {
         val names = arrayOf("AAA", "BCD", "EFG", "HIJ", "KLM", "NOP")
         for (name in names) {
