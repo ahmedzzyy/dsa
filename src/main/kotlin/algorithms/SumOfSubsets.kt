b19671ab97e1963e5852fcebd6ac00125718e982
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-03-11 15:14:04 +0530

    Refactor: Updated KDoc for `subsetsWithSum`

--- src/main/kotlin/algorithms/SumOfSubsets.kt
+++ src/main/kotlin/algorithms/SumOfSubsets.kt
@@ -1,5 +1,13 @@
 package edu.practice.algorithms
 
+/**
+ * Computes all subsets of the given list of integers that sum exactly to the specified target.
+ *
+ * @param sum The total sum the subset should amount to
+ * @param numbers [List] of [Int] to form subsets from
+ * @return List of subsets which amount to [sum]
+ */
+// "form" subsets "from" He he :-)
 fun subsetsWithSum(sum: Int, numbers: List<Int>): List<List<Int>> {
     if (numbers.isEmpty()) return emptyList()
 
